b1725e2eb6148ec2ea99f97acba64b618202970e
goder1 <113771449+goder1@users.noreply.github.com>
2022-11-17 12:27:16 +0300

    just copying files

---
+++ Node.h
@@ -0,0 +1,206 @@
+#ifndef Node_h
+#define Node_h
+
+#include <stdio.h>
+#include <iostream>
+#include <stdlib.h>
+#include <ctime>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+
+struct Node_elem
+{
+    int a;
+    Node_elem* next;
+    Node_elem* prev;
+};
+
+class Node
+{
+    Node_elem* Pnode;
+    Node_elem* current;
+public:
+
+    Node()
+    {
+        Pnode = nullptr;
+        current = nullptr;
+    }
+
+    Node(int n)
+    {
+        if (n == 0)
+        {
+            Pnode = nullptr;
+            current = nullptr;
+        }
+        else
+        {
+            Pnode = new Node_elem;
+            current = Pnode;
+            Node_elem* ptr = Pnode;
+            ptr->prev = nullptr;
+            for (int i = 0; i < n; i++)
+            {
+                ptr->a = rand() % 4;
+                if(i + 1 < n)
+                {
+                    ptr->next = new Node_elem;
+                    ptr->next->prev = ptr;
+                    ptr = ptr->next;
+                }
+            }
+            ptr->next = nullptr;
+        }
+    }
+
+    bool Is_Empty()
+    {
+        if (Pnode == nullptr)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    void Move_current_forward()
+    {
+        current = current->next;
+    }
+
+    void Move_current_backward()
+    {
+        current = current->prev;
+    }
+
+    void Push_front(int elem)
+    {
+        Node_elem* ptr = new Node_elem;
+        ptr->a = elem;
+        ptr->next = Pnode;
+        ptr->prev = nullptr;
+        Pnode->prev = ptr;
+        Pnode = ptr;
+    }
+
+    void Push_back(int elem)
+    {
+        Node_elem* ptr = Pnode;
+        while (ptr->next != nullptr)
+        {
+            ptr = ptr->next;
+        }
+        ptr->next = new Node_elem;
+        ptr->next->prev = ptr;
+        ptr = ptr->next;
+        ptr->a = elem;
+        ptr->next = nullptr;
+    }
+
+    void Pop_front()
+    {
+        if (Pnode->next != nullptr)
+        {
+        Node_elem* ptr = Pnode->next;
+        ptr->prev = nullptr;
+        delete Pnode;
+        Pnode = ptr;
+        }
+        else
+        {
+            delete Pnode;
+            Pnode = nullptr;
+            current = nullptr;
+        }
+    }
+
+    void Pop_back()
+    {
+        if (Pnode->next != nullptr)
+        {
+            Node_elem* ptr = Pnode;
+            while (ptr->next->next != nullptr)
+            {
+                ptr = ptr->next;
+            }
+            delete ptr->next;
+            ptr->next = nullptr;
+        }
+        else
+        {
+            delete Pnode;
+            Pnode = nullptr;
+            current = nullptr;
+        }
+    }
+
+    void Insert_current(int x)
+    {
+        if(current->prev != nullptr)
+        {
+            current->prev->next = new Node_elem;
+            current->prev->next->a = x;
+            current->prev->next->next = current;
+            current->prev = current->prev->next;
+        }
+        else
+        {
+            Node_elem* ptr = new Node_elem;
+            ptr->a = x;
+            ptr->next = Pnode;
+            ptr->prev = nullptr;
+            Pnode->prev = ptr;
+            Pnode = ptr;
+        }
+    }
+
+    void Delete_current()
+    {
+        Node_elem* ptr1 = current->prev;
+        Node_elem* ptr2 = current->next;
+        delete current;
+        ptr1->next = ptr2;
+        ptr2->prev = ptr1;
+        current = ptr1;
+    }
+
+
+    Node operator=(Node node)
+    {
+        Node A;
+        A.Pnode = node.Pnode;
+        return A;
+    }
+
+    ~Node()
+    {
+        Node_elem* ptr = Pnode;
+        while (ptr != nullptr)
+        {
+            ptr = ptr->next;
+            delete Pnode;
+            Pnode = ptr;
+        }
+    }
+
+    friend ostream& operator<<(ostream&, const Node&);
+};
+
+
+ostream& operator<<(ostream& out, const Node& A)
+{
+    for (Node_elem* i = A.Pnode; i != nullptr; i = i->next)
+    {
+        out << i->a << " ";
+    }
+    out << endl;
+    return out;
+}
+
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,218 @@
+#include <stdio.h>
+#include <iostream>
+#include <stdlib.h>
+#include <ctime>
+#include <vector>
+#include "Node.h"
+#include <cstdlib>
+#include <unistd.h>
+
+using namespace std;
+
+int main()
+{
+    srand(time(NULL));
+    int a,b,c,d;
+    string w;
+    string command = "";
+    int n = 25 + rand % 100;
+    Node node(n);
+    int current;
+    if(n)
+    {
+        current = 0;
+    }
+    else
+    {
+        current = -1;
+    }
+    cout<<"1.PLAY\n2.BOT_PLAYING DEMO\n";
+    string choice;
+    cin >> choice;
+    if(int(choice[0] - 48 == 1 && choice.length() == 1)
+    {
+        const int window_height = 4;
+        int window_width;
+        cout << "Enter Width of game_window (not less than 8)\n";
+        cin >> window_width;
+        system("clear");
+        while (command != "STOP")
+        {
+            node.print();
+            cout << "Current balloon is: "<<current_ballon<< endl << "(Next balloon is: " << next_balloon << ")" << endl;
+            cout << "1.Push_front\n2.Push_back\n3.Pop_front\n4.Pop_back\n5.Insert num\n6.Delete num\n7.Is_empty?\n8.STOP\n\n Choose the command:\n";
+            cin >> w;
+            if(int(w[0]) - 48 == 1 && w.size() == 1)
+            {
+                cout << "Enter the number which you want to insert:\n";
+                int elem;
+                cin >> elem;
+                node.Push_front(elem);
+                current++;
+            }
+
+
+            else if(int(w[0]) - 48 == 2 && w.size() == 1)
+            {
+                cout << "Enter the number which you want to insert:\n";
+                int elem;
+                cin >> elem;
+                node.Push_back(elem);
+            }
+
+
+            else if (int(w[0]) - 48 == 3 && w.size() == 1)
+            {
+                node.Pop_front();
+                if (current > 0)
+                {
+                    current--;
+                }
+            }
+
+
+            else if (int(w[0]) - 48 == 4 && w.size() == 1)
+            {
+                node.Pop_back();
+                if (current == n-1)
+                {
+                    current--;
+                }
+            }
+
+            else if (int(w[0]) - 48 == 5 && w.size() == 1)
+            {
+                cout << "Enter the number which you want to insert:\n";
+                int x;
+                cin >> x;
+                node.Insert_current(x);
+                current++;
+            }
+
+            else if (int(w[0]) - 48 == 6 && w.size() == 1)
+            {
+                node.Delete_current();
+                current--;
+            }
+
+            else if (int(w[0]) - 48 == 7 && w.size() == 1)
+            {
+                if (node.Is_Empty())
+                {
+                cout << "True\n";
+                }
+                else
+                {
+                    cout << "False\n";
+                }
+                sleep(2);
+            }
+
+            else if (int(w[0]) - 48 == 8 && w.size() == 1)
+            {
+                command = "STOP";
+                return 0;
+            }
+
+
+            else
+            {
+                cout << "strange command :/\n\n";
+            }
+
+            system("clear");
+        }
+    return 0;
+    }
+    else if(int(choice[0] - 48 == 2 && choice.length() == 1)
+    {
+        system("clear");
+    while (command != "STOP")
+    {
+        cout << node << endl;
+        cout << "Current elem is: "<<current<< endl << endl;
+        cout << "1.Push_front\n2.Push_back\n3.Pop_front\n4.Pop_back\n5.Insert num\n6.Delete num\n7.Is_empty?\n8.STOP\n\n Choose the command:\n";
+        cin >> w;
+        if(int(w[0]) - 48 == 1 && w.size() == 1)
+        {
+            cout << "Enter the number which you want to insert:\n";
+            int elem;
+            cin >> elem;
+            node.Push_front(elem);
+            current++;
+        }
+
+
+        else if(int(w[0]) - 48 == 2 && w.size() == 1)
+        {
+            cout << "Enter the number which you want to insert:\n";
+            int elem;
+            cin >> elem;
+            node.Push_back(elem);
+        }
+
+
+        else if (int(w[0]) - 48 == 3 && w.size() == 1)
+        {
+            node.Pop_front();
+            if (current > 0)
+            {
+                current--;
+            }
+        }
+
+
+        else if (int(w[0]) - 48 == 4 && w.size() == 1)
+        {
+            node.Pop_back();
+            if (current == n-1)
+            {
+                current--;
+            }
+        }
+
+        else if (int(w[0]) - 48 == 5 && w.size() == 1)
+        {
+            cout << "Enter the number which you want to insert:\n";
+            int x;
+            cin >> x;
+            node.Insert_current(x);
+            current++;
+        }
+
+        else if (int(w[0]) - 48 == 6 && w.size() == 1)
+        {
+            node.Delete_current();
+            current--;
+        }
+
+        else if (int(w[0]) - 48 == 7 && w.size() == 1)
+        {
+            if (node.Is_Empty())
+            {
+             cout << "True\n";
+            }
+            else
+            {
+                cout << "False\n";
+            }
+            sleep(2);
+        }
+
+        else if (int(w[0]) - 48 == 8 && w.size() == 1)
+        {
+            command = "STOP";
+            return 0;
+        }
+
+
+        else
+        {
+            cout << "strange command :/\n\n";
+        }
+
+        system("clear");
+    }
+    return 0;
+    }
+}
